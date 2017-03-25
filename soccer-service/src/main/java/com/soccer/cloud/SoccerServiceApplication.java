4efeedcb1455cec9629d5ac99ba4b6b95fbd2e2d
Anatoly Papenko <privettoli@gmail.com>
2017-03-25 23:29:43 +0200

    [SCRS-0004] Add eureka-service

--- soccer-service/src/main/java/com/soccer/cloud/SoccerServiceApplication.java
+++ soccer-service/src/main/java/com/soccer/cloud/SoccerServiceApplication.java
@@ -2,9 +2,11 @@ package com.soccer.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public interface SoccerServiceApplication {
     static void main(String... args) {
