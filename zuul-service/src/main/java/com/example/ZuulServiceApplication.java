ffed9428a3bd0d7e3ba078b6a9de708d33044397
Anatoly Papenko <privettoli@gmail.com>
2017-03-25 23:49:35 +0200

    [SCRS-0005] Add Zuul-service

---
+++ zuul-service/src/main/java/com/example/ZuulServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public interface ZuulServiceApplication {
+    static void main(String... args) {
+        SpringApplication.run(ZuulServiceApplication.class, args);
+    }
+}
